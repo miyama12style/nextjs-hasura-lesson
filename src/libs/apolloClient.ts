147da6e840d117219281b324ee798144580ac7fd
mahiro miyazaki <mahiro.siawasemono@ezweb.ne.jp>
2022-05-13 00:36:51 +0900

    🎨 Next.jsとhasuraの連携

---
+++ src/libs/apolloClient.ts
@@ -0,0 +1,31 @@
+// Next.jsとhasuraを連携させる
+// createApolloClientでapolloインスタンスや、initializeApolloでクライアントサイドとサーバーサイドを切り分ける初期化に関する処理を作成
+
+import { ApolloClient, HttpLink, InMemoryCache, NormalizedCacheObject } from '@apollo/client'
+
+export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
+
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({
+      uri: 'https://trusted-piglet-28.hasura.app/v1/graphql', // Server URL (must be absolute)
+    }),
+    cache: new InMemoryCache(),
+  })
+}
+
+export const  initializeApollo = () => {
+  const _apolloClient = apolloClient ?? createApolloClient()
+
+  // For SSG and SSR always create a new Apollo Client
+  // サーバーサイドの処理 毎回apolloClientを返さないといけない
+  if (typeof window === 'undefined') return _apolloClient
+  // Create the Apollo Client once in the client
+  // クライアントサイドの処理 一回実行したらいい
+  if (!apolloClient) apolloClient = _apolloClient
+
+  return _apolloClient
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import 'src/styles/globals.css'
 import { AppProps } from 'next/app'
 
+// hasuraと連携させるため
+import { ApolloProvider } from '@apollo/client'
+import { initializeApollo } from 'src/libs/apolloClient'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // クライアントを作成
+  const client = initializeApollo()
+
+  return (
+    // どんなコンポーネントでも使えるように
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
