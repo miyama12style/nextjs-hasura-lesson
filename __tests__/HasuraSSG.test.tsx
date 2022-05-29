96c156799a8b3c79bd5f24d5f1fe8f2cb8fd4d20
mahiro miyazaki <mahiro.siawasemono@ezweb.ne.jp>
2022-05-29 19:30:38 +0900

    ✅ userページのテスト

---
+++ __tests__/HasuraSSG.test.tsx
@@ -0,0 +1,37 @@
+import { cleanup, render, screen } from '@testing-library/react'
+import { setupServer } from 'msw/node'
+import { getPage, initTestHelpers } from 'next-page-tester'
+import { handlers } from 'src/mock/handlers'
+import '@testing-library/jest-dom/extend-expect'
+
+initTestHelpers()
+
+const server = setupServer(...handlers)
+
+beforeAll(() => {
+  // サーバーを起動
+  server.listen()
+})
+afterEach(() => {
+  // 各テストが終了したら、サーバーをリセットして、
+  server.resetHandlers()
+  // testing-libraryをcleanupする
+  cleanup()
+})
+afterAll(() => {
+  // テストが終了したら、サーバーを閉じる
+  server.close()
+})
+
+describe('hasuraSSGのテストです', () => {
+  it('hasuraSSGのレンダリングのテスト', async () => {
+    const { page } = await getPage({
+      route: '/hasura-ssg',
+    })
+    render(page)
+    expect(await screen.findByText('SSG + ISR')).toBeInTheDocument()
+    expect(screen.getByText('Test user A')).toBeInTheDocument()
+    expect(screen.getByText('Test user B')).toBeInTheDocument()
+    expect(screen.getByText('Test user C')).toBeInTheDocument()
+  })
+})
