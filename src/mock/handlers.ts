756a0965c8f66ea320c47d88346f63634a6cd5aa
mahiro miyazaki <mahiro.siawasemono@ezweb.ne.jp>
2022-05-31 01:57:35 +0900

    ⚡️

--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -7,10 +7,6 @@ import { graphql } from 'msw'
 export const handlers = [
   // 第一引数にqueryのフィールド名→apolloClientなどを使って、アプリのコード内でGetUsersを呼んでいるところが、handlersで定義したテスト用のデータに置き換わってくれる
 
-  
-  
-  
-
   // userの情報を取ってくる
   graphql.query('GetUsers', (req, res, ctx) => {
     return res(
@@ -46,15 +42,15 @@ export const handlers = [
         users: [
           {
             __typename: 'users',
-            id: 'fcb4302a-5597-4e3e-995b-19180ba39daf',
+            id: '64293d80-ca56-7cb6-f00a-ae0699b4385d',
           },
           {
             __typename: 'users',
-            id: 'f8f7fc18-4410-40fa-a6ca-778562bc1799',
+            id: 'fcd77df9-cb0e-7876-eb4a-782e01ff16cb',
           },
           {
             __typename: 'users',
-            id: '804b9aeb-567f-466e-9695-f17358585525',
+            id: '9dce62aa-86e4-b29b-06f9-15d138fd85be',
           },
         ],
       })
@@ -66,36 +62,36 @@ export const handlers = [
     // 渡されたIDを格納する
     const { id } = req.variables
 
-    if (id === 'fcb4302a-5597-4e3e-995b-19180ba39daf') {
+    if (id === '64293d80-ca56-7cb6-f00a-ae0699b4385d') {
       return res(
         ctx.data({
           users_by_pk: {
             __typename: 'users',
-            id: 'fcb4302a-5597-4e3e-995b-19180ba39daf',
+            id: '64293d80-ca56-7cb6-f00a-ae0699b4385d',
             name: 'Test user A',
             created_at: '2022-05-06T10:41:04.365422+00:00',
           },
         })
       )
     }
-    if (id === 'f8f7fc18-4410-40fa-a6ca-778562bc1799') {
+    if (id === 'fcd77df9-cb0e-7876-eb4a-782e01ff16cb') {
       return res(
         ctx.data({
           users_by_pk: {
             __typename: 'users',
-            id: 'f8f7fc18-4410-40fa-a6ca-778562bc1799',
+            id: 'fcd77df9-cb0e-7876-eb4a-782e01ff16cb',
             name: 'Test user B',
             created_at: '2022-05-06T10:41:21.08985+00:00',
           },
         })
       )
     }
-    if (id === '804b9aeb-567f-466e-9695-f17358585525') {
+    if (id === '9dce62aa-86e4-b29b-06f9-15d138fd85be') {
       return res(
         ctx.data({
           users_by_pk: {
             __typename: 'users',
-            id: '804b9aeb-567f-466e-9695-f17358585525',
+            id: '9dce62aa-86e4-b29b-06f9-15d138fd85be',
             name: 'Test user C',
             created_at: '2022-05-06T10:41:26.729478+00:00',
           },
