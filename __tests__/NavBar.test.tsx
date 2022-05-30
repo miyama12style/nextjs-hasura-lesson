a1d3b452a99ec557e0b79318bb4c80b9bab0ed4f
mahiro miyazaki <mahiro.siawasemono@ezweb.ne.jp>
2022-05-30 19:05:01 +0900

    ⚰️

--- __tests__/NavBar.test.tsx
+++ __tests__/NavBar.test.tsx
@@ -9,7 +9,6 @@ import { setupServer } from 'msw/node'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen, cleanup } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import 'setimmediate'
 
 //next-page-testerを使う場合は、最初に初期化する必要がある
 initTestHelpers()
